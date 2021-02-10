bd1145cb8c7fac4f8eea1c77cbd9b7c0e9a09c0e
Jeff Chiu <jwchiu82@gmail.com>
2021-02-09 20:39:56 -0800

    add functional ticket creation

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -7,9 +7,18 @@ class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
+      masterTicketList: []
     };
   }
+
+handleAddingNewTicketToList = (newTicket) => {
+  const newMasterTicketList = this.state.masterTicketList.concat(newTicket);
+  this.setState({
+    masterTicketList: newMasterTicketList,
+    formVisibleOnPage: false
+  });
+}
 
   handleClick = () => {
     this.setState(prevState => ({
@@ -21,10 +30,10 @@ class TicketControl extends React.Component {
     let currentlyVisibleState = null;
     let buttonText = null;
     if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTicketForm/>
+      currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList}/> 
       buttonText = "Return to Ticket List";
     } else {
-      currentlyVisibleState = <TicketList/>
+      currentlyVisibleState = <TicketList ticketList={this.state.masterTicketList}/>;
       buttonText = "Add Ticket";
     }
     return (
