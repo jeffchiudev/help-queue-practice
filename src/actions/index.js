2f0061024f00fc20cc810c92c192baf29948f8a4
Jeff Chiu <jwchiu82@gmail.com>
2021-02-17 19:54:57 -0800

    green pass updateTime

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,6 +9,12 @@ export const toggleForm = () => ({
   type: c.TOGGLE_FORM,
 });
 
+export const updateTime = (id, formattedWaitTime) => ({
+  type: c.UPDATE_TIME,
+  id: id,
+  formattedWaitTime: formattedWaitTime
+});
+
 export const addTicket = (ticket) => {
   const { names, location, issue, id } = ticket;
   return {
